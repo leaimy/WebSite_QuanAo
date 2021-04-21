84c8aaaaf70034b477f84f5d7e721c4e33d91e41
LeAiMy <hantctk42@gmail.com>
2021-04-21 16:50:39 +0700

    Khởi tạo route hiển thị giao diện 🐛

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Frontend.Home.index');
 });
+
+Route::get('/admin', function () {
+    return view('Backend.Dashboard.index');
+});
+
