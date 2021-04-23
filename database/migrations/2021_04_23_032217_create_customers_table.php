2638830f94d644c4a9d5808a3d8c220f15700571
LeAiMy <hantctk42@gmail.com>
2021-04-23 10:47:22 +0700

    Chen bang trong csdl ❄❄❄

---
+++ database/migrations/2021_04_23_032217_create_customers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone_number')->unique()->nullable();
+            $table->string('street')->nullable();
+            $table->string('village')->nullable();
+            $table->string('district')->nullable();
+            $table->string('province')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
