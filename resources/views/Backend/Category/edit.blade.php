51d2928efb589b31e3ad54153db4adf0fd581755
LeAiMy <hantctk42@gmail.com>
2021-04-26 16:37:34 +0700

    Cập nhật giao diện chỉnh sửa sản phẩm ❄

--- resources/views/Backend/Category/edit.blade.php
+++ resources/views/Backend/Category/edit.blade.php
@@ -1,149 +1,64 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Dashboard 2</title>
+@extends('Backend.app')
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="{{ asset('backend/plugins/fontawesome-free/css/all.min.css') }}">
-    <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="{{ asset('backend/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('backend/dist/css/adminlte.min.css') }}">
-</head>
-<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-<div class="wrapper">
+@section('content-header')
+    Quản lý nhóm sản phẩm ❄
+@endsection
 
-    <!-- Preloader -->
-@include('Backend._preloader')
-
-<!-- Navbar -->
-@include('Backend._navbar')
-<!-- /.navbar -->
-
-    <!-- Main Sidebar Container -->
-@include('Backend._main-sidebar')
-
-<!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">Quản lý nhóm sản phẩm</h1>
-                    </div><!-- /.col -->
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Quản lý nhóm sản phẩm</li>
-                        </ol>
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
-        </div>
-        <!-- /.content-header -->
-
-        <!-- Main content -->
-        <section class="content">
-            <div class="container-fluid">
-                <div class="row justify-content-center">
-                    <div class="col-md-8">
-                        <div class="card card-primary">
-                            <div class="card-header">
-                                <h3 class="card-title">Thêm nhóm sản phẩm</h3>
-                            </div>
-                            <!-- /.card-header -->
-                            <!-- form start -->
-                            <form action="{{route('AdminCategory.update',['id'=>$category['id']])}}" method="post">
-                                @csrf
-
-                                <div class="card-body">
-                                    <div class="form-group">
-                                        <label for="exampleInputEmail1">Tên nhóm</label>
-                                        <input value="{{$category['name']}}" name="name" type="text" class="form-control" id="exampleInputEmail1"
-                                               placeholder="Nhập tên nhóm">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="exampleSelectRounded0">Chọn nhóm</label>
-                                        <select  name="parent_id" class="custom-select rounded-0"
-                                                id="exampleSelectRounded0">
-                                            <option value="0">Chọn nhóm cha</option>
-                                            @foreach($parents as $parent)
-                                                <option @if($category['parent_id']==$parent['id']) selected="selected" @endif value="{{$parent['id']}}">{{$parent['name']}}</option>
-
-                                            @endforeach
-                                        </select>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="customRadio5">Trạng thái hiển thị</label>
-                                        <div class="custom-control custom-radio">
-                                            <input value="1" @if($category['status']==1) checked="checked" @endif
-                                                   class="custom-control-input custom-control-input-danger custom-control-input-outline"
-                                                   type="radio" id="customRadio5" name="status">
-                                            <label for="customRadio5" class="custom-control-label">Hiển thị</label>
-                                        </div>
-                                        <div class="custom-control custom-radio">
-                                            <input value="0" @if($category['status']==0) checked="checked" @endif
-                                                   class="custom-control-input custom-control-input-danger custom-control-input-outline"
-                                                   type="radio" id="nocustomRadio5" name="status">
-                                            <label for="nocustomRadio5" class="custom-control-label">Không hiển
-                                                thị</label>
-                                        </div>
-                                    </div>
-
-                                </div>
-                                <!-- /.card-body -->
-
-                                <div class="card-footer">
-                                    <button type="submit" class="btn btn-primary">Submit</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+@section('content-body')
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">Thêm nhóm sản phẩm</h3>
                 </div>
-            </div><!--/. container-fluid -->
-        </section>
-        <!-- /.content -->
+                <!-- /.card-header -->
+                <!-- form start -->
+                <form action="{{route('AdminCategory.update',['id'=>$category['id']])}}" method="post">
+                    @csrf
+
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Tên nhóm</label>
+                            <input value="{{$category['name']}}" name="name" type="text" class="form-control" id="exampleInputEmail1"
+                                   placeholder="Nhập tên nhóm">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleSelectRounded0">Chọn nhóm</label>
+                            <select  name="parent_id" class="custom-select rounded-0"
+                                     id="exampleSelectRounded0">
+                                <option value="0">Chọn nhóm cha</option>
+                                @foreach($parents as $parent)
+                                    <option @if($category['parent_id']==$parent['id']) selected="selected" @endif value="{{$parent['id']}}">{{$parent['name']}}</option>
+
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="customRadio5">Trạng thái hiển thị</label>
+                            <div class="custom-control custom-radio">
+                                <input value="1" @if($category['status']==1) checked="checked" @endif
+                                class="custom-control-input custom-control-input-danger custom-control-input-outline"
+                                       type="radio" id="customRadio5" name="status">
+                                <label for="customRadio5" class="custom-control-label">Hiển thị</label>
+                            </div>
+                            <div class="custom-control custom-radio">
+                                <input value="0" @if($category['status']==0) checked="checked" @endif
+                                class="custom-control-input custom-control-input-danger custom-control-input-outline"
+                                       type="radio" id="nocustomRadio5" name="status">
+                                <label for="nocustomRadio5" class="custom-control-label">Không hiển
+                                    thị</label>
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- /.card-body -->
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-    <!-- /.content-wrapper -->
-
-    <!-- Control Sidebar -->
-@include('Backend._control-sidebar')
-<!-- /.control-sidebar -->
-
-    <!-- Main Footer -->
-    @include('Backend._main-footer')
-</div>
-<!-- ./wrapper -->
-
-<!-- REQUIRED SCRIPTS -->
-<!-- jQuery -->
-<script src="{{ asset('backend/plugins/jquery/jquery.min.js') }}"></script>
-<!-- Bootstrap -->
-<script src="{{ asset('backend/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<!-- overlayScrollbars -->
-<script src="{{ asset('backend/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
-<!-- AdminLTE App -->
-<script src="{{ asset('backend/dist/js/adminlte.js') }}"></script>
-
-<!-- PAGE PLUGINS -->
-<!-- jQuery Mapael -->
-<script src="{{ asset('backend/plugins/jquery-mousewheel/jquery.mousewheel.js') }}"></script>
-<script src="{{ asset('backend/plugins/raphael/raphael.min.js') }}"></script>
-<script src="{{ asset('backend/plugins/jquery-mapael/jquery.mapael.min.js') }}"></script>
-<script src="{{ asset('backend/plugins/jquery-mapael/maps/usa_states.min.js') }}"></script>
-<!-- ChartJS -->
-<script src="{{ asset('backend/plugins/chart.js/Chart.min.js') }}"></script>
-
-<!-- AdminLTE for demo purposes -->
-<script src="{{ asset('backend/dist/js/demo.js') }}"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{ asset('backend/dist/js/pages/dashboard2.js') }}"></script>
-</body>
-</html>
+@endsection
