1b1683b7d00cc53d28904d4a5daa1e017e0c045b
LeAiMy <hantctk42@gmail.com>
2021-04-28 15:03:31 +0700

    Bổ sung file data 🍸🍸🍸🍸

--- data.sql
+++ data.sql
@@ -3,12 +3,11 @@ INSERT INTO categories (id, name, slug, status, parent_id) VALUES (2, 'quần', 
 INSERT INTO categories (id, name, slug, status, parent_id) VALUES (3, 'váy', 'vay', 0, 0);
 INSERT INTO categories (id, name, slug, status, parent_id) VALUES (4, 'áo sơ mi', 'ao-so-mi', 1, 1);
 INSERT INTO categories (id, name, slug, status, parent_id) VALUES (5, 'áo thun', 'ao-thun', 1, 1);
-INSERT INTO categories (id, name, slug, status, parent_id) VALUES (6, 'áo khoác', 'ao-khoac', 1, 1);
-INSERT INTO categories (id, name, slug, status, parent_id) VALUES (7, 'áo croptop', 'ao-croptop', 1, 1);
-INSERT INTO categories (id, name, slug, status, parent_id) VALUES (8, 'quần kaki', 'quan-kaki', 1, 2);
-INSERT INTO categories (id, name, slug, status, parent_id) VALUES (9, 'quần jean', 'quan-jean', 1, 2);
-INSERT INTO categories (id, name, slug, status, parent_id) VALUES (10, 'quần vải', 'quan-vai', 1, 2);
-INSERT INTO categories (id, name, slug, status, parent_id) VALUES (11, 'váy dài', 'vay-dai', 1, 3);
-INSERT INTO categories (id, name, slug, status, parent_id) VALUES (12, 'váy ngắn', 'vay-ngan', 1, 3);
-INSERT INTO categories (id, name, slug, status, parent_id) VALUES (13, 'chân váy', 'chan-vay', 1, 3);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (6, 'áo len', 'ao-len', 1, 1);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (7, 'quần kaki', 'quan-kaki', 1, 2);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (8, 'quần jean', 'quan-jean', 1, 2);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (9, 'quần thun', 'quan-thun', 1, 2);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (10, 'váy dài', 'vay-dai', 1, 3);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (11, 'váy ngắn', 'vay-ngan', 1, 3);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (12, 'chân váy', 'chan-vay', 1, 3);
 
