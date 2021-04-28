74ce455010bc174044e94990ca2775d456a47c13
LeAiMy <hantctk42@gmail.com>
2021-04-28 09:49:24 +0700

    Bo sung file data 🐙🐙🐙🐙

---
+++ data.sql
@@ -0,0 +1,14 @@
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (1, 'áo', 'ao', 0, 0);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (2, 'quần', 'quan', 0, 0);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (3, 'váy', 'vay', 0, 0);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (4, 'áo sơ mi', 'ao-so-mi', 1, 1);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (5, 'áo thun', 'ao-thun', 1, 1);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (6, 'áo khoác', 'ao-khoac', 1, 1);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (7, 'áo croptop', 'ao-croptop', 1, 1);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (8, 'quần kaki', 'quan-kaki', 1, 2);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (9, 'quần jean', 'quan-jean', 1, 2);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (10, 'quần vải', 'quan-vai', 1, 2);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (11, 'váy dài', 'vay-dai', 1, 3);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (12, 'váy ngắn', 'vay-ngan', 1, 3);
+INSERT INTO categories (id, name, slug, status, parent_id) VALUES (13, 'chân váy', 'chan-vay', 1, 3);
+
