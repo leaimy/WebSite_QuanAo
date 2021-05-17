3fccbfe1d7a2a45827fc7f8bc6be1fd99b154533
DalatCoder <hieuntctk42@gmail.com>
2021-05-17 17:17:52 +0700

    Cam kết khách hàng ❤

--- resources/views/Frontend/Home/index.blade.php
+++ resources/views/Frontend/Home/index.blade.php
@@ -2110,24 +2110,24 @@
                     <div class="row">
                         <div class="col-lg-4 col-md-6 u-s-m-b-30">
                             <div class="service u-h-100">
-                                <div class="service__icon"><i class="fas fa-truck"></i></div>
+                                <div class="service__icon"><i class="fas fa-balance-scale"></i></div>
                                 <div class="service__info-wrap">
 
-                                    <span class="service__info-text-1">Free Shipping</span>
+                                    <span class="service__info-text-1">Uy tín</span>
 
                                     <span
-                                        class="service__info-text-2">Free shipping on all US order or order above $200</span>
+                                        class="service__info-text-2">Sản phầm quần áo uy tín, chất lượng, có nguồn gốc xuất xứ rõ ràng</span>
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-4 col-md-6 u-s-m-b-30">
                             <div class="service u-h-100">
-                                <div class="service__icon"><i class="fas fa-redo"></i></div>
+                                <div class="service__icon"><i class="fas fa-truck"></i></div>
                                 <div class="service__info-wrap">
 
-                                    <span class="service__info-text-1">Shop with Confidence</span>
+                                    <span class="service__info-text-1">Nhanh chóng</span>
 
-                                    <span class="service__info-text-2">Our Protection covers your purchase from click to delivery</span>
+                                    <span class="service__info-text-2">Giao hàng nhanh chóng, tận nơi, tiện lợi</span>
                                 </div>
                             </div>
                         </div>
@@ -2136,9 +2136,9 @@
                                 <div class="service__icon"><i class="fas fa-headphones-alt"></i></div>
                                 <div class="service__info-wrap">
 
-                                    <span class="service__info-text-1">24/7 Help Center</span>
+                                    <span class="service__info-text-1">Hỗ trợ khách hàng</span>
 
-                                    <span class="service__info-text-2">Round-the-clock assistance for a smooth shopping experience</span>
+                                    <span class="service__info-text-2">Nhân viên hỗ trợ khách hàng 24/7, tận tâm, nhiệt tình</span>
                                 </div>
                             </div>
                         </div>
