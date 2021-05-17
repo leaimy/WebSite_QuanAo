0252ce758241876f55fc5737d139641db6e0493f
DalatCoder <hieuntctk42@gmail.com>
2021-05-17 20:45:38 +0700

    Thêm dữ liệu phần nhận xét khách hàng 🐇

--- data.sql
+++ data.sql
@@ -15,3 +15,14 @@ INSERT INTO users(id, first_name, last_name, username, email, password, avatar_n
 VALUES (1, 'Hiếu', 'Nguyễn Trọng', 'tronghieu', 'hieuntctk42@gmail.com', '$2y$10$goP9dyeKfbaRZeVzqfdXhehPFasiEo0FMnvZIB1WJHgWrQQwqZys2', 'avatar1.jpg', 'images/avatars/avatar1.jpg');
 INSERT INTO users(id, first_name, last_name, username, email, password, avatar_name, avatar_path)
 VALUES (2, 'Hà', 'Nguyễn Thị', 'thiha', 'hantctk42@gmail.com', '$2y$10$6phFUH9t5GSlnd50i0YS8ugYKwfEik9uwgXTxmrsFP9tfwdWKcKXa', 'avatar2.jpg', 'images/avatars/avatar2.jpg');
+
+INSERT INTO client_feedbacks(id, content, author_info, image_name, image_path)
+VALUES (1, 'Sản phẩm uy tín, chất lượng, tôi rất là ưng cái bụng', 'Meow khó tính', 'review1.jpg', 'images/reviews/review1.jpg');
+INSERT INTO client_feedbacks(id, content, author_info, image_name, image_path)
+VALUES (2, 'Không thể tin được, cửa hàng thật tuyệt vời, thật không thể tin được', 'Meow thật không thể tin được', 'review2.jpg', 'images/reviews/review2.jpg');
+INSERT INTO client_feedbacks(id, content, author_info, image_name, image_path)
+VALUES (3, 'Sản phẩm tuyệt vời, tôi không thể ngừng việc mua sắm tại Shop Bạch Tuyết', 'Meow thời trang', 'review3.jpg', 'images/reviews/review3.jpg');
+INSERT INTO client_feedbacks(id, content, author_info, image_name, image_path)
+VALUES (4, 'Nhân viên tư vấn nhiệt tình, tận tâm. Bà chủ dễ thương số 1', 'Meow thân thiện', 'review4.jpg', 'images/reviews/review4.jpg');
+INSERT INTO client_feedbacks(id, content, author_info, image_name, image_path)
+VALUES (5, 'Lại bắt viết review nữa à, bực cái mình nà, mèo méo meo mèo meo', 'Meow cục súc', 'review5.jpg', 'images/reviews/review5.jpg');
